f20a9d9248a1e30c3542b06c06dead0ddb6c0bf5
indy-greg <gregstephens92@gmail.com>
2017-01-16 15:03:23 +0000

    First Commit
    First Commit

---
+++ RDPG/RDPG/CustomEntries/HomeButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RDPG.CustomEntries
+{
+    public class HomeButton : Entry
+    {
+        public HomeButton()
+        {
+            BackgroundColor = Color.Green;
+        }
+    }
+}
---
+++ RDPG/RDPG/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RDPG
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void minSkillsButton_Clicked(object sender, EventArgs e)
+        {
+            var minSkillsPage = new MinSkills();
+            Navigation.PushAsync(minSkillsPage, true);           
+        }
+    }
+}
